3eb22bbd7c607872fdf0308dbafb98e86230eb5c
ethanc <17chiue@gmail.com>
2019-07-04 00:20:07 -0400

    Refactor and optimize code

--- js/javascript.js
+++ js/javascript.js
@@ -23,7 +23,9 @@ $( document ).ready(function() {
 
 //Edited https://stackoverflow.com/questions/30906807/word-frequency-in-javascript
 function wordFreq(string) {
-    var words = string.replace(/[.]/g, '').split(/\s/);
+		// get rid of urls, @ mentions, punctionation + spaces
+		var no_url = string.replace(/(?:https?|ftp):\/\/[\n\S]+/g, '').replace(/\S*@\S*\s?/g, "").replace(/(?:(the|a|an|and|of|what|to|for|about) +)/g, "").replace(/[.,\/#!$%\^&\*;:{}=\-_`~()]/g,"");
+    var words = no_url.replace(/[.]/g, '').split(/\s/);
     var freqMap = {};
     words.forEach(function(w) {
         if (!freqMap[w]) {
@@ -35,156 +37,36 @@ function wordFreq(string) {
     return freqMap;
 }
 
+function generateWordFreqs(raw_data, name) {
+	var tweets = [];
+	for(var i = 0; i<raw_data.length; i++){
+		tweets.push(raw_data[i].innerText);
+	}
+	//Regex to elimate tags, etc. + join words into one coherent group
+	tweets = tweets.join(' ');
+
+	listA = wordFreq(tweets);
+	var list = [];
+	for (var key in listA)
+	{
+		list.push([key, listA[key]*5]);
+	}
+	WordCloud.minFontSize = "15px";
+	WordCloud($('#' + name + '_word_cloud')[0], { list: list });
+}
+
 function generateWordCloud(){
 	setTimeout(
 	    function() {
 	    	//Set up variables, get document values from iframes loaded asyncronously
-	    	var count = [];
-	    	var liberal = document.getElementById('twitter-widget-0').contentWindow.document;
-	    	var moderate = document.getElementById('twitter-widget-1').contentWindow.document;
-	    	var conservative = document.getElementById('twitter-widget-2').contentWindow.document;
-	    	// console.log(liberal)
-
-	    	//Get Tweets from each iframe
-	    	liberalTweetsHTML = $(liberal).find(".timeline-Tweet-text");
-	    	count.push(liberalTweetsHTML.length);
-	    	moderateTweetsHTML = $(moderate).find(".timeline-Tweet-text");
-	    	count.push(moderateTweetsHTML.length);
-	    	conservativeTweetsHTML = $(conservative).find(".timeline-Tweet-text");
-	    	count.push(conservativeTweetsHTML.length);
-
-	    	// console.log(count);
-	    	//If there are less than 10 tweets for any of the feeds, recursively run this function until there are 10 tweets.
-	    	if(count[0] < 10 || count[1] < 10 || count[2] < 10)
-	    	{
-	    		generateWordCloud();
-	    		return;
-	    	}
-	    	
-	    	//Extract text from Tweets
-	    	var liberalTweets =[];
-	    	for(var i = 0; i<liberalTweetsHTML.length; i++){
-	    		liberalTweets.push(liberalTweetsHTML[i].innerText);
-	    	}
-	    	var moderateTweets =[];
-	    	for(var i = 0; i<moderateTweetsHTML.length; i++){
-	    		moderateTweets.push(moderateTweetsHTML[i].innerText);
-	    	}
-	    	var conservativeTweets =[];
-	    	for(var i = 0; i<conservativeTweetsHTML.length; i++){
-	    		conservativeTweets.push(conservativeTweetsHTML[i].innerText);
-	    	}
-	    	// console.log(liberalTweets);
-	    	// console.log(moderateTweets);
-	    	// console.log(conservativeTweets);
+				//Work around for not being able to explicitly call $("#twitter-widget-.....")
+	    	var liberal = $(document.getElementById('twitter-widget-0').contentWindow.document).find(".timeline-Tweet-text");
+	    	var moderate = $(document.getElementById('twitter-widget-1').contentWindow.document).find(".timeline-Tweet-text");
+	    	var conservative = $(document.getElementById('twitter-widget-2').contentWindow.document).find(".timeline-Tweet-text");
 
 	    	//Generate Word Clouds
-	    	//Regex to elimate tags, etc. + join words into one coherent group
-	    	liberalTweets = liberalTweets.join(' ');
-	    	moderateTweets = moderateTweets.join(' ');
-	    	conservativeTweets = conservativeTweets.join(' ');
-
-	    	// console.log(liberalTweets);
-	    	//Get rid of @ mentions
-	    	liberalTweets = liberalTweets.replace(/\S*@\S*\s?/g, "");
-	    	//Get rid of links
-	    	liberalTweets = liberalTweets.replace(/(http|ftp|https):\/\/([\w_-]+(?:(?:\.[\w_-]+)+))([\w.,@?^=%&:/~+#-]*[\w@?^=%&/~+#-])?/g,"");
-	    	// console.log(liberalTweets);
-	    	//Get rid of punctionation + spaces
-	    	var punctuationless = liberalTweets.replace(/[.,\/#!$%\^&\*;:{}=\-_`~()]/g,"");
-			liberalTweets = punctuationless.replace(/\s{2,}/g," ");
-			//Get rid of articles of speech
-			regex = /(?:(the|a|an|and|of|what|to|for|about) +)/g; 
-			liberalTweets = liberalTweets.replace(regex, "");
-	    	// liberalTweets = liberalTweets.split(" ");
-	    	// console.log(liberalTweets);
-	    	listA = wordFreq(liberalTweets);
-	    	var list = [];
-	    	for (var key in listA)
-	    	{
-	    		list.push([key, listA[key]*5]);
-	    	}
-	    	console.log(list);
-			console.log(WordCloud.isSupported);
-			WordCloud.minFontSize = "15px";
-			WordCloud($('#liberal_word_cloud')[0], { list: list });
-
-			moderateTweets = moderateTweets.replace(/\S*@\S*\s?/g, "");
-	    	//Get rid of links
-	    	moderateTweets = moderateTweets.replace(/(http|ftp|https):\/\/([\w_-]+(?:(?:\.[\w_-]+)+))([\w.,@?^=%&:/~+#-]*[\w@?^=%&/~+#-])?/g,"");
-	    	// console.log(liberalTweets);
-	    	//Get rid of punctionation + spaces
-	    	var punctuationless = moderateTweets.replace(/[.,\/#!$%\^&\*;:{}=\-_`~()]/g,"");
-			moderateTweets = punctuationless.replace(/\s{2,}/g," ");
-			//Get rid of articles of speech
-			regex = /(?:(the|a|an|of|and|what|to|for|about) +)/g; 
-			moderateTweets = moderateTweets.replace(regex, "");
-	    	// liberalTweets = liberalTweets.split(" ");
-	    	// console.log(liberalTweets);
-	    	listA = wordFreq(moderateTweets);
-	    	var list = [];
-	    	for (var key in listA)
-	    	{
-	    		list.push([key, listA[key]*5]);
-	    	}
-	    	console.log(list);
-			console.log(WordCloud.isSupported);
-			WordCloud.minFontSize = "15px";
-			WordCloud($('#moderate_word_cloud')[0], { list: list });
-
-			conservativeTweets = conservativeTweets.replace(/\S*@\S*\s?/g, "");
-	    	//Get rid of links
-	    	conservativeTweets = conservativeTweets.replace(/(http|ftp|https):\/\/([\w_-]+(?:(?:\.[\w_-]+)+))([\w.,@?^=%&:/~+#-]*[\w@?^=%&/~+#-])?/g,"");
-	    	// console.log(liberalTweets);
-	    	//Get rid of punctionation + spaces
-	    	var punctuationless = conservativeTweets.replace(/[.,\/#!$%\^&\*;:{}=\-_`~()]/g,"");
-			conservativeTweets = punctuationless.replace(/\s{2,}/g," ");
-			//Get rid of articles of speech
-			regex = /(?:(the|a|an|and|of|what|to|for|about) +)/g; 
-			conservativeTweets = conservativeTweets.replace(regex, "");
-	    	// liberalTweets = liberalTweets.split(" ");
-	    	// console.log(liberalTweets);
-	    	listA = wordFreq(conservativeTweets);
-	    	var list = [];
-	    	for (var key in listA)
-	    	{
-	    		list.push([key, listA[key]*5]);
-	    	}
-	    	console.log(list);
-			console.log(WordCloud.isSupported);
-			WordCloud.minFontSize = "15px";
-			WordCloud($('#conservative_word_cloud')[0], { list: list });
-
-
-
-
-
-	    	// var allLiberal = liberalTweets.split(" ");
-	    	// var allModerate = moderateTweets.split(" ");
-	    	// var allConservative = conservativeTweets.split(" ");
-
-	    	
+				generateWordFreqs(liberal, "liberal");
+				generateWordFreqs(moderate, "moderate");
+				generateWordFreqs(conservative, "conservative");
 	    }, 250);
 }
-
-// $('#liberal').bind('load', function() {
-// 	 console.log(document.getElementById('twitter-widget-0').contentWindow.document);
-// });
-// function resolveAfter2Seconds() {
-//   return new Promise(resolve => {
-//     setTimeout(() => {
-//       resolve('resolved');
-//     }, 2000);
-//   });
-// }
-
-// async function asyncCall() {
-//   console.log('calling');
-//   var result = await resolveAfter2Seconds();
-//   console.log(result);
-//   var libtweets = $(".timeline-Tweet-text")
-// 	console.log(libtweets);
-// 	console.log(document.getElementById('twitter-widget-0').contentWindow.document);
-//   // expected output: "resolved"
-// }
-
